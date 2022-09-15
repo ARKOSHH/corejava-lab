a53d847151518bb4f4adab7a7c7ace552932be40
ARKOSHH <113019125+ARKOSHH@users.noreply.github.com>
2022-09-15 18:20:39 +0530

    Add files via upload

---
+++ 15 sep/Charecterr.java
@@ -0,0 +1,33 @@
+class Charecterr
+
+{
+ public static void main(String[] args)
+ {
+ 
+   char Charecter ='i';
+ 
+ switch (Charecter)
+ 
+  {
+    case'a':
+	       System.out.println(Charecter+" is a vowel");
+		   break;
+	case'e':
+	       System.out.println(Charecter+"is a vowel");
+		   break;
+	case'i':
+	        System.out.println(Charecter+" is a vowel");
+		    break;
+	case'o':
+	         System.out.println(Charecter+" is a vowel");
+		     break;
+	case'u': 
+	         System.out.println(Charecter+" is a vowel");
+		     break;
+	default:
+	      System.out.println(Charecter+ " is a consonant");
+		   
+	}
+  }
+
+}
